3231442d359bf5e1aa6e0aef6bde8b3e8fb74dfa
Sultan Mahmud <kaziiriad@gmail.com>
2023-12-20 16:15:16 +0600

    Added admin functionality

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Task, Image
 # Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'description', 'priority', 'status')
+    ordering = ('priority')
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+
+    list_display = ('images')
